52673a9d7ac744a7b08747de2f2658830262377f
mculotta120 <margaret.culotta@gmail.com>
2016-07-08 10:11:22 -0500

    yaaaaaaay! the nested schema is finally working.  there is a json file available for structure reference.

--- models/fa_issueColl.js
+++ models/fa_issueColl.js
@@ -6,18 +6,18 @@ var fa_issueSchema = new mongoose.Schema({  // set up new mongoose schema
   issue_number: Number,
   issue_name: String,
   issue_thumbnail: String,
-  issue_pages: [{ type: Schema.Types.Mixed, ref: 'arrayPages' }]
+  issue_pages: [{"default" : Schema.Types.Mixed  }]
 });
 
-var pagesArraySchema = new mongoose.Schema({  // set up new mongoose schema
-  _id : { type: Number, ref: 'fa_issues' },
-  page_number: Number,
-  page_location: String,
-  page_thumbnail: String,
-  // issue_pages: panelsArraySchema
-});
+// var pagesArraySchema = new mongoose.Schema({  // set up new mongoose schema
+//   _id : { type: Number, ref: 'fa_issues' },
+//   page_number: Number,
+//   page_location: String,
+//   page_thumbnail: String,
+//   // issue_pages: panelsArraySchema
+// });
 
 var fa_issues = mongoose.model( 'fa_issues', fa_issueSchema );  // sets schema to model var
-var arrayPages = mongoose.model( 'arrayPages', pagesArraySchema );
+// var arrayPages = mongoose.model( 'arrayPages', pagesArraySchema );
 // module.exports= arrayPages;
 module.exports=fa_issues;
